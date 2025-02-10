c27c724da5f0e0095cd972d2b0ab1a1890810213
Pritu Yadav <101568121+prituyadav@users.noreply.github.com>
2025-02-10 15:57:06 +0530

    initial commit

---
+++ live-language-translator/src/main/java/com/pritu/translator/LiveLanguageTranslatorApplication.java
@@ -0,0 +1,13 @@
+package com.pritu.translator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveLanguageTranslatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveLanguageTranslatorApplication.class, args);
+	}
+
+}
---
+++ live-language-translator/src/test/java/com/pritu/translator/LiveLanguageTranslatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pritu.translator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveLanguageTranslatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
